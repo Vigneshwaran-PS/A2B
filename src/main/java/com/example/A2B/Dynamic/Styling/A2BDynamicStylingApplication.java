3a3b30a6c96a517ac57b1f00e1a7b659489f99ed
Vigneshwaran-PS <psviki29@gmail.com>
2024-04-04 15:07:48 +0530

    application properties updated

--- src/main/java/com/example/A2B/Dynamic/Styling/A2BDynamicStylingApplication.java
+++ src/main/java/com/example/A2B/Dynamic/Styling/A2BDynamicStylingApplication.java
@@ -2,9 +2,10 @@ package com.example.A2B.Dynamic.Styling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class A2BDynamicStylingApplication {
+public class A2BDynamicStylingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(A2BDynamicStylingApplication.class, args);
